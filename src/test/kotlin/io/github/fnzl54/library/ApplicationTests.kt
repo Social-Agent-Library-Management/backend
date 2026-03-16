ee8f76788fe9af89bb0497f3dfb4ace19c5ec165
fnzl54 <rnjscksdud1@naver.com>
2026-03-17 08:56:58 +0900

    initial commit

---
+++ src/test/kotlin/io/github/fnzl54/library/ApplicationTests.kt
@@ -0,0 +1,11 @@
+package io.github.fnzl54.library
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
