330f89a279a47ffca005a18f612aa5635148e070
chanyoung <rnjscksdud1@naver.com>
2026-08-06 16:14:47 +0900

    feat(#3): 도서 삭제 API 추가

---
+++ src/main/kotlin/org/library/book/application/DeleteBookService.kt
@@ -0,0 +1,23 @@
+package org.library.book.application
+
+import org.library.book.domain.BookRepository
+import org.library.book.domain.error.BookError
+import org.library.core.application.Result
+import org.library.core.application.err
+import org.library.core.application.ok
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class DeleteBookService(
+    private val bookRepository: BookRepository,
+) {
+
+    @Transactional
+    fun execute(id: Long): Result<Unit, BookError> {
+        val book = bookRepository.findByIdAndDeletedAtIsNull(id)
+            ?: return BookError.NOT_FOUND.err()
+        book.softDelete()
+        return Unit.ok()
+    }
+}
