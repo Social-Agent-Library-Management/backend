4c2372768cf4691616c4dd2c24798060699e4495
chanyoung <rnjscksdud1@naver.com>
2026-08-08 22:22:14 +0900

    feat(#15): 대출 등록 API 추가

--- src/main/kotlin/org/library/bookitem/domain/BookItemRepository.kt
+++ src/main/kotlin/org/library/bookitem/domain/BookItemRepository.kt
@@ -1,8 +1,16 @@
 package org.library.bookitem.domain
 
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface BookItemRepository : JpaRepository<BookItem, Long> {
 
     fun findByManagementNumber(managementNumber: String): BookItem?
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select bi from BookItem bi where bi.managementNumber = :managementNumber")
+    fun findByManagementNumberForUpdate(@Param("managementNumber") managementNumber: String): BookItem?
 }
