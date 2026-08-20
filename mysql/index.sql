7d4ef4f0b0e477cee6183f6787e5f0e26da00e00
chanyoung <rnjscksdud1@naver.com>
2026-08-20 14:58:08 +0900

    feat(#31): 도서 검색 MySQL FULLTEXT 옵션 추가

--- mysql/index.sql
+++ mysql/index.sql
@@ -7,3 +7,7 @@ CREATE UNIQUE INDEX ux_book_active_isbn
 # 관리번호는 전체 고유해야 한다. (check-then-act의 동시 요청 레이스 컨디션을 DB 유니크 제약으로 방어)
 CREATE UNIQUE INDEX ux_book_item_management_number
     ON book_item (management_number);
+
+# 사용처 : BookRepository.searchFullText (search.engine=mysql_fulltext)
+# 한글은 공백 기준 형태소 분리가 무의미해 ngram 파서(기본 ngram_token_size=2) 기반 역색인을 쓴다.
+ALTER TABLE book ADD FULLTEXT INDEX idx_book_title_author_ft (title, author) WITH PARSER ngram;
