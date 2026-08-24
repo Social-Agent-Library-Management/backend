d29b546c043cf7a309200189321cc44acc2e809b
chanyoung <rnjscksdud1@naver.com>
2026-08-24 10:37:36 +0900

    feat(#35): 도서 등록,수정,삭제 시 색인 이벤트 발행

--- src/main/kotlin/org/library/book/application/UpdateBookService.kt
+++ src/main/kotlin/org/library/book/application/UpdateBookService.kt
@@ -2,6 +2,7 @@ package org.library.book.application
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
+import org.library.book.application.event.BookChangedEvent
 import org.library.book.domain.entity.Book
 import org.library.book.domain.repository.BookRepository
 import org.library.book.domain.error.BookError
@@ -9,12 +10,14 @@ import org.library.book.dto.BookResponse
 import org.library.core.application.Result
 import org.library.core.application.err
 import org.library.core.application.ok
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UpdateBookService(
     private val bookRepository: BookRepository,
+    private val applicationEventPublisher: ApplicationEventPublisher,
 ) {
 
     @Transactional
@@ -31,6 +34,7 @@ class UpdateBookService(
         }
 
         book.update(title = request.title, author = request.author, isbn = isbn, publisher = request.publisher)
+        applicationEventPublisher.publishEvent(BookChangedEvent(book.id))
         return BookResponse.from(book).ok()
     }
 
